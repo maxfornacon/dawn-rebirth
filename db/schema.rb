e1beb548ad8ff93e21e69b24945c9acb009fe925
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2016-10-01 13:06:31 +0200

    trying to push to dokku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905101403) do
+ActiveRecord::Schema.define(version: 20160924214714) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20160905101403) do
     t.boolean  "admin",                  default: false
     t.integer  "score"
     t.integer  "guildrank",              default: 0
+    t.boolean  "online",                 default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
