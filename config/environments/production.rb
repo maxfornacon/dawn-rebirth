2f6294b1363d5ef53959a6a6533a9d5045b86324
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2016-10-09 22:44:26 +0200

    second try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,9 @@ Rails.application.configure do
 	config.action_cable.allowed_request_origins = ['http://dare.pakass.de']
 
   config.paperclip_defaults = {
-    :storage => :minio,
-    :bucket => 'dawnrebirth'
+    :storage => :s3,
+    :s3_region => 'us-east-1',
+    :s3_host_name => 'win.pakass.de:9000',
+    :bucket => 'devdawnrebirth'
   }
 end
