f43a87fa4147542ceba2baf46c18ea2b745047cf
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2017-06-18 15:49:35 +0200

    add images to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ApplicationRecord
 	belongs_to :user
 	validates :description, presence: true, length: {maximum: 222}
 
-	has_attached_file :image, styles: { crop: "365x"}
+	has_attached_file :image, styles: { crop: "325x"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	has_many :flags, dependent: :destroy
