cc3b4d69d1a10c1c80215a8a9bd938d136ddb59c
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2017-06-21 22:41:24 +0200

    add image, yt, and link highlighting in chat

--- app/models/msg.rb
+++ app/models/msg.rb
@@ -2,4 +2,11 @@ class Msg < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
   scope :for_display, -> { order(:created_at).last(50)}
+  auto_html_for :content do
+		html_escape
+		image
+		youtube(width: 300, height: 200 )
+		link target: '_blank', rel: 'nofollow'
+		simple_format
+	end
 end
