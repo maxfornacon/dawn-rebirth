19cc53227f4ecbd6c7303194bf20da07e7cdd4f5
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2017-06-22 10:31:20 +0200

    improved animekanban

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,5 +1,5 @@
 class Anime < ApplicationRecord
 	belongs_to :user
-	validates :name, presence: true, length: {minimum: 2, maximum: 20}
+	validates :name, presence: true, length: {minimum: 2, maximum: 100}
 end
                              
