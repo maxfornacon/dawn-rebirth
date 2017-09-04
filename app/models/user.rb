32d0c7ecff6a6ff7631a922c69a0fa8cac9fa797
Maximilian Fornaçon <maximilian.fornacon@gmail.com>
2017-09-05 01:47:57 +0200

    add personal messaging

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,12 @@ class User < ApplicationRecord
   has_many :updates
   
   acts_as_voter
+  
+  has_many :authored_conversations, class_name: 'Conversation', foreign_key: 'author_id'
+  has_many :received_conversations, class_name: 'Conversation', foreign_key: 'received_id'
+
+  has_many :personal_messages, dependent: :destroy
+
 
   def online?
     !Redis.new.get("user_#{self.id}_online").nil?
