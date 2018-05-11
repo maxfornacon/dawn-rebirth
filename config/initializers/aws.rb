7f5b967c0425814edfa1758dc3745f5fc67e5406
Max <maximilian.fornacon@gmail.com>
2018-05-11 21:03:25 +0200

    added aws env_vars

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,7 +3,7 @@ Aws.config.update(
   endpoint: "http://176.9.106.52:9000",
   force_path_style: true,
   credentials: Aws::Credentials.new(
-    "Z7RZPYZQS7Z9AWI0E6NU",
-    "gHBEKZPtXnAFXSS/83DoQRBYq1kxg4VkF3czGYCB"
+    "ENV['MINIO_KEY1']",
+    "ENV['MINIO_KEY2']"
   )
 )
